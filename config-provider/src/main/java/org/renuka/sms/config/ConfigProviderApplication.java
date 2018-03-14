e73b3dc69c7f381748da6cd624939161585c6f0d
Renuka Fernando <renukapiyumal@gmail.com>
2018-03-14 20:50:15 +0000

    initialize project

---
+++ config-provider/src/main/java/org/renuka/sms/config/ConfigProviderApplication.java
@@ -0,0 +1,13 @@
+package org.renuka.sms.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigProviderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigProviderApplication.class, args);
+    }
+}
