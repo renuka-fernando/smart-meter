27f92a87fb3a0f9ffc6d62b7ffa15a0102c76dd1
Renuka Fernando <renuka.piyumal@gmail.com>
2018-09-08 17:10:20 +0530

    add pre filters for API gateway

--- gateway/src/main/java/org/renuka/sms/gateway/GatewayApplication.java
+++ gateway/src/main/java/org/renuka/sms/gateway/GatewayApplication.java
@@ -1,13 +1,20 @@
 package org.renuka.sms.gateway;
 
+import org.renuka.sms.gateway.filter.pre.AuthFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringBootApplication
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public AuthFilter authFilter(){
+        return new AuthFilter();
     }
 }
---
+++ gateway/src/main/java/org/renuka/sms/gateway/filter/pre/AuthFilter.java
@@ -0,0 +1,37 @@
+package org.renuka.sms.gateway.filter.pre;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class AuthFilter extends ZuulFilter {
+    private static Logger log = LoggerFactory.getLogger(AuthFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext currentContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = currentContext.getRequest();
+        log.info("RENUKA: " + request.getMethod());
+        log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+
+        return null;
+    }
+}
