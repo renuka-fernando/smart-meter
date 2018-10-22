a3ef702f9d9690a0801df5529a21e3a8209c1bfe
Renuka Fernando <renuka.piyumal@gmail.com>
2018-10-22 14:42:01 +0530

    add temporary CORS origins

--- smart-meter-services/account-service/src/main/java/org/renuka/sms/account/api/AccountAPI.java
+++ smart-meter-services/account-service/src/main/java/org/renuka/sms/account/api/AccountAPI.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
+@CrossOrigin(origins = {"http://localhost:3000"})
 @RestController
 @RequestMapping("/accounts")
 public class AccountAPI {
--- smart-meter-services/account-service/src/main/java/org/renuka/sms/account/api/CustomerAPI.java
+++ smart-meter-services/account-service/src/main/java/org/renuka/sms/account/api/CustomerAPI.java
@@ -12,18 +12,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.HashMap;
 
+@CrossOrigin(origins = {"http://localhost:3000"})
 @RestController
 @RequestMapping("/customers")
 public class CustomerAPI {
